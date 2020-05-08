4d27830ee44d20383ea47f1b2ecf648a11e42944
Sourabh607 <sourabhinfo1@gmail.com>
2020-05-08 17:06:38 +0530

    second commit

--- src/main/scala/org/apache/spark/constants/PriceConstants.scala
+++ src/main/scala/org/apache/spark/constants/PriceConstants.scala
@@ -2,6 +2,6 @@ package org.apache.spark.constants
 
 object PriceConstants {
 
-  val file_Path: String = "E:/DataSamples/csv/train.csv"
+  val file_Path: String = "E:/DataSamples/csv/salesdata.csv"
   val winutils_File_Path: String = "E://DataSamples//hadoop"
 }
